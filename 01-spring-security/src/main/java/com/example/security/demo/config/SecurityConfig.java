a19573c31c02ba28f4cbff437f52428f139b3c1a
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-10 11:52:26 +0200

    Add form-based authentication examples

--- 01-spring-security/src/main/java/com/example/security/demo/config/SecurityConfig.java
+++ 01-spring-security/src/main/java/com/example/security/demo/config/SecurityConfig.java
@@ -12,10 +12,50 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.security.demo.security.UserRole.*;
 
+
+/*
+ * Basic auth:
+ * - (in header) Authorization: Basic <Base64 encoded -> username:password>
+ * - HTTPS recommended
+ * - Simple and fast
+ * - Can't logout
+ *
+ * Client            Server
+ *  GET request ----->
+ * <-----Unauthorized---
+ *
+ * ---GET request | Base64 username:password--
+ * <-------200 OK----------
+ *
+ *
+ * Form-based auth:
+ * - Username and password
+ * - Standard in most websites
+ * - Forms (full control)
+ * - Can logout
+ * - HTTPS recommended
+ *
+ * Client            Server
+ * ----POST username password----
+ *                               |
+ *                               |
+ *     validates credentials<-----
+ * <-------OK--------------------
+ * <-------COOKIE SESSIONID------
+ *
+ * Any request with SESSIONID----->
+ *                                |
+ *      validates SESSIONID<-------
+ * <--------OK---------------------
+ *
+ * -SESSIONID expires after 30 mins of inactivity, can be prolonged with remember me
+ * */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true) // for using annotation-based authorization
@@ -28,31 +68,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
-    // 1. when the clients signs in, it receives CSRF (Cross-Site Request Forgery) token, which prevents
-    // csrf attacks.
-    // 2. When the user submits the form, the client sends CSRF token in the request.
-    // 3.the server validates the token.
-    // NOTE: GET requests work with CSRF, but POST/PUT/DELETE are protected
-
-    /*WHEN TO USE CSRF PROTECTION?
-     * By Spring docs:
-     * for any request being used by normal users
-     * if we have service that is used only by non-signed-in users, then we don't need it*/
-    // X-XSRF-TOKEN (got from GET request)
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable() // disable csrf
-                // disable accessing to cookies from some client-side scripts
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .and()
+                .csrf().disable() // disable csrf
                 .authorizeRequests()
                 .antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name()) // allow only students to access student resources
                 .anyRequest() //-> any path
                 .authenticated()
                 .and()
-                .httpBasic();
+//                .httpBasic(); // -> for basic auth
+                // session id is stored in-memory, but we can use e.g. Postgres or Redis
+                .formLogin() //  -> for form-based auth
+                    .loginPage("/login")
+                    .permitAll() // custom login page
+                    .defaultSuccessUrl("/courses", true) // redirect after successful login
+                    .usernameParameter("username") // default value => username, can be customized
+                    .passwordParameter("password") // default value => password, can be customized
+                .and()
+                .rememberMe() // defaults to 2 weeks; remember-me cookie, also stored in the database
+                // (by default stored in-memory)
+                // remember-me cookie:
+                // - username
+                // - expiration time
+                // - MD5 of these two values
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // set expiration time frame
+                    .key("somethingverysecure") // custom key used to hash with MD5
+                    .rememberMeParameter("remember-me") // default value => remember-me, can be customized
+                .and()
+                .logout()
+                    .logoutUrl("/logout") // default url is /logout
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // this happens
+                    // under the hood when CSRF protection is disabled. If it is enabled, this line should be remove
+                    .clearAuthentication(true) // clear auth
+                    .invalidateHttpSession(true) // invalidate session
+                    .deleteCookies("JSESSIONID", "remember-me") // delete session cookies
+                    .logoutSuccessUrl("/login");
+        // if CSRF protection is enabled, logout request must be POST
+        // otherwise, any HTTP method is allowed
+
+
     }
 
     @Override
